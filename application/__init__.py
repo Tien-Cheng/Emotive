adcc84452ca71f7318f45af3d0198aea9d9e248f
Tien-Cheng <tiencheng@protonmail.com>
2022-01-30 12:49:05 +0000

    [feat] Implement postgresql integration

--- application/__init__.py
+++ application/__init__.py
@@ -6,19 +6,27 @@ import os
 
 # Create the Flask app
 app = Flask(__name__)
-CORS(app) # Enable CORS
+CORS(app)  # Enable CORS
 
 if "TESTING" in os.environ:
-    app.config.from_envvar('TESTING')
+    app.config.from_envvar("TESTING")
     print("Using config for TESTING")
+elif "DEVELOPMENT" in os.environ:
+    app.config.from_envvar("DEVELOPMENT")
+    print("Using config for DEVELOPMENT")
 else:
-    app.config.from_pyfile('config.cfg')
-    print(">>> Using default config")
+    app.config.from_pyfile("config_deploy.cfg")
+    print("Using Deployment configuration")
+
+# Fix to get Heroku PostgreSQL to work
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri is not None and uri.startswith("postgres://"):
+    os.environ["DATABASE_URL"] = uri.replace("postgres://", "postgresql://", 1)
 
 login_manager = LoginManager(app)
- 
+
 # Instantiate SQLAlchemy to handle db process
 db = SQLAlchemy(app)
 
-#run the file routes.py
+# run the file routes.py
 from application import routes
