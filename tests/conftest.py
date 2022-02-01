41318fd3c9a54ae4223396b82a479a85d42eb3f6
Tien-Cheng <tiencheng@protonmail.com>
2022-02-01 19:35:06 +0800

    [test] Add populate users fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,42 @@
+from datetime import datetime as dt
+
 import pytest
 from app import app as flask_app
+from application import db
 
 
 @pytest.fixture
 def app():
     yield flask_app
 
+
 @pytest.fixture
 def client(app):
     print(app.static_url_path)
     yield app.test_client()
+
+
+@pytest.fixture
+def populate_users():
+    users = [
+        {
+            "username": "user_1@example.com",
+            "password": "Password1234!",
+        },
+        {
+            "username": "user_2@example.com",
+            "password": "Password567890!",
+        },
+    ]
+    for user in users:
+        try:
+            db.session.add(
+                User(
+                    username=user["email"],
+                    password_hash=user["password"],
+                    created=dt.utcnow(),
+                )
+            )
+            db.session.commit()
+        except:
+            db.session.rollback()
