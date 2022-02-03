13da01e0344e27189684fcf3853921cd357f934d
Tien-Cheng <tiencheng@protonmail.com>
2022-02-03 17:48:38 +0800

    [fix] Enforce entity integrity constraint on User model

--- application/models.py
+++ application/models.py
@@ -60,7 +60,7 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
     
     id         = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username   = db.Column(db.String, nullable=False)
+    username   = db.Column(db.String, nullable=False, unique=True)
     password   = db.Column(db.String, nullable=False)
     created_on = db.Column(db.DateTime, nullable=False)
 
