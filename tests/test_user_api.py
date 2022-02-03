7e8dc643e901680a4da72477a6937e37a2573ed3
Tien-Cheng <tiencheng@protonmail.com>
2022-02-03 17:49:47 +0800

    [test] Implement basic user management unit tests

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -21,8 +21,9 @@ def test_user_add_api(client, userlist, capsys):
 
         # Send post request to API
         response = client.post(
-            "/api/user-add/", data=json.dumps(data), content_type="application/json"
+            "/api/user-add", data=json.dumps(data), content_type="application/json"
         )
+
 
         response_body = json.loads(response.get_data(as_text=True))
 
@@ -38,26 +39,69 @@ def test_user_add_api(client, userlist, capsys):
 
 @pytest.mark.usefixtures("populate_users")
 @pytest.mark.parametrize(
-    "id_list",
-    [3, 4],
+    "userlist",
+    [1, 2],
 )
 def test_user_get_api(client, userlist, capsys):
     with capsys.disabled():
-        raise NotImplementedError
+        # Send post request to API
+        response = client.get(
+            f"/api/user-get/{userlist}", content_type="application/json"
+        )
+        print(response.get_data(as_text=True))
+        print(response.status_code)
+        response_body = json.loads(response.get_data(as_text=True))
 
+        # Check response status code
+        assert response.status_code == 200
+
+        # Check consistent content type
+        assert response.headers["Content-Type"] == "application/json"
+
+        # Check result is consistent with what was sent
+        assert response_body["id"] == userlist , "User ID was not correctly returned"
+        assert response_body["username"], "Username was not returned"
+        assert response_body["password"], "Password was not returned"
 
 
 @pytest.mark.usefixtures("populate_users")
-def test_user_get_all_api(client, userlist, capsys):
+def test_user_get_all_api(client, capsys):
     with capsys.disabled():
-        raise NotImplementedError
+        # Send post request to API
+        response = client.get("/api/get-users", content_type="application/json")
+        response_body = json.loads(response.get_data(as_text=True))
+        print(response_body)
+        # Check response status code
+        assert response.status_code == 200
 
+        # Check consistent content type
+        assert response.headers["Content-Type"] == "application/json"
+
+        # Check response
+        for user in response_body:
+            assert user["id"], "User ID was not correctly returned"
+            assert user["username"], "Username was not returned"
+            assert user["password"], "Password was not returned"
+        assert len(response_body) == 4 # 2 from populate_users and 2 from add user unit test
 
 @pytest.mark.usefixtures("populate_users")
 @pytest.mark.parametrize(
-    "id_list",
-    [3, 4],
+    "userlist",
+    [1, 2],
 )
 def test_user_delete_api(client, userlist, capsys):
     with capsys.disabled():
-        raise NotImplementedError
+        # Send post request to API
+        response = client.get(
+            f"/api/user-delete/{userlist}", content_type="application/json"
+        )
+        response_body = json.loads(response.get_data(as_text=True))
+
+        # Check response status code
+        assert response.status_code == 200
+
+        # Check consistent content type
+        assert response.headers["Content-Type"] == "application/json"
+
+        # Check result is consistent with what was sent
+        assert response_body["result"] == "ok"
