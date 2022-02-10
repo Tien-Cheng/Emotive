1b333916fef5b4edc1cdc5c90fa9dcdd202fe118
Muhammad Faqih Akmal <62086734+AqilAkmal@users.noreply.github.com>
2022-02-10 22:40:28 +0800

    [feat] Added no face and more than one faces test

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -2,12 +2,13 @@ import pytest
 import json
 import os
 from io import BytesIO
+import numpy as np
 
 emotion_list = ("angry", "fearful", "surprised", "happy", "neutral", "sad", "disgusted")
 
 # Test predict API
 @pytest.mark.parametrize("emotionImg", [f"{e}.{ext}" for e in emotion_list for ext in ['png', 'jpg']])
-def test_predictAPI(client, emotionImg, capsys):
+def test_predAPI(client, emotionImg, capsys):
     with capsys.disabled():
 
         data = {
@@ -27,13 +28,8 @@ def test_predictAPI(client, emotionImg, capsys):
         # Remove test api images
         os.remove(f"./application/static/images/{response_body['file_path']}")
         assert "error" not in response_body.keys()
-
-        
-        # Hacky way to check if prediction is correct
-        if emotionImg.split(".")[0] in ["disgusted", "fearful"]:
-            assert response_body["emotion"] in ["angry", "disgusted", "neutral"]
-        else:
-            assert response_body["emotion"] == emotionImg.split(".")[0]
+       
+        assert emotionImg.split('.')[0].capitalize() in np.array(response_body["prediction"])[:,0][:4]
 
 # Test non png or non jpg files
 @pytest.mark.xfail(reason="Not png or jpg")
@@ -42,5 +38,27 @@ def test_predictAPI(client, emotionImg, capsys):
     "emotion.pdf",
     "happy.webp"
 ])
-def test_predictAPI_fail(client, files, capsys):
-    test_predictAPI(client, files, capsys)
+def test_predAPI_NonImg(client, files, capsys):
+    test_predAPI(client, files, capsys)
+
+# Test for no face detected
+@pytest.mark.xfail(reason="No face detected")
+@pytest.mark.parametrize("files", [
+    "room.jpg",
+    "sky.jpg"
+    "mountain.jpg",
+    "ocean.jpg",
+    "forest.jpg",
+])
+def test_predAPI_NoFace(client, files, capsys):
+    test_predAPI(client, files, capsys)
+
+# Test more than one face
+@pytest.mark.xfail(reason="More than one face")
+@pytest.mark.parametrize("files", [
+    "two_faces.jpg",
+    "two_faces_2.jpg",
+    "two_faces_3.jpg"
+])
+def test_predAPI_MoreThanOneFace(client, files, capsys):
+    test_predAPI(client, files, capsys)
