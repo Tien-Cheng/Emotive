a92dfc4362a842a838b013f0b00d4cbfc7af7ae1
Muhammad Faqih Akmal <62086734+AqilAkmal@users.noreply.github.com>
2022-02-11 01:01:43 +0800

    [feat] Consistency test for predict

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -28,7 +28,7 @@ def test_predAPI(client, emotionImg, capsys):
         # Remove test api images
         os.remove(f"./application/static/images/{response_body['file_path']}")
         assert "error" not in response_body.keys()
-       
+
         assert emotionImg.split('.')[0].capitalize() in np.array(response_body["prediction"])[:,0][:4]
 
 # Test non png or non jpg files
@@ -45,10 +45,10 @@ def test_predAPI_NonImg(client, files, capsys):
 @pytest.mark.xfail(reason="No face detected")
 @pytest.mark.parametrize("files", [
     "room.jpg",
-    "sky.jpg"
+    "sky.jpg",
     "mountain.jpg",
     "ocean.jpg",
-    "forest.jpg",
+    "forest.jpg"
 ])
 def test_predAPI_NoFace(client, files, capsys):
     test_predAPI(client, files, capsys)
@@ -62,3 +62,35 @@ def test_predAPI_NoFace(client, files, capsys):
 ])
 def test_predAPI_MoreThanOneFace(client, files, capsys):
     test_predAPI(client, files, capsys)
+
+# Consistency test
+@pytest.mark.parametrize("emotionImg", [f"{e}.{ext}" for e in emotion_list for ext in ['png', 'jpg']])
+def test_predAPI_Consistency(client, emotionImg, capsys):
+    with capsys.disabled():
+
+        responses = []
+
+        for _ in range(2):
+
+            data = {
+                "file": (BytesIO(open(f'./tests/test_files/{emotionImg}', 'rb').read()), emotionImg)
+            }
+            
+            response = client.post(
+                '/api/predict',
+                data=data,
+                content_type="multipart/form-data"
+            )
+
+            assert response.status_code == 200
+            assert response.headers["Content-Type"] == "application/json"
+            response_body = json.loads(response.get_data(as_text=True))
+            
+            # Remove test api images
+            os.remove(f"./application/static/images/{response_body['file_path']}")
+            assert "error" not in response_body.keys()
+
+            responses.append(response_body)
+
+        assert responses[0]["emotion"] == responses[1]["emotion"]
+        assert responses[0]["prediction"] == responses[1]["prediction"]
