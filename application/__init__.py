bfb43649c78de1007be978294e750ac1450570ab
Tien-Cheng <tiencheng@protonmail.com>
2022-04-07 16:22:36 +0800

    [fix] Fix incorrect handling of secret key

--- application/__init__.py
+++ application/__init__.py
@@ -27,7 +27,7 @@ elif "DEVELOPMENT" in os.environ:
 
 else:
     app.config.from_pyfile("config_deploy.cfg")
-    app.config.fromenvvar("SECRET_KEY") # Stored securely in Heroku
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "local_testing") # Stored securely in Heroku
     print("Using Deployment configuration")
 
 # Fix to get Heroku PostgreSQL to work
